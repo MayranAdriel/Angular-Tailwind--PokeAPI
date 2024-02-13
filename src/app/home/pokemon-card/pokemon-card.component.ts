76455690aad5190d4c9a5d92639b64a1290a1bec
Mayran Adriel <mayranalbuquerque@gmail.com>
2024-02-13 08:53:24 -0300

    tratamento de error httpClient

--- src/app/home/pokemon-card/pokemon-card.component.ts
+++ src/app/home/pokemon-card/pokemon-card.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonService } from '../../services/pokemon.service';
 import { FormsModule } from '@angular/forms';
-import { stat } from 'node:fs';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -16,17 +15,15 @@ export class PokemonCardComponent implements OnInit {
 
   pokemons: any[] = [];
 
-
   getPokemon: string = '';
-
 
   pokemon = {
     name: '',
     imageUrl: '',
     types: [],
     numberId: 0,
-    statsName: [],
-    statsNumber: []
+    statsName: '',
+    statsNumber: 0,
   };
 
   ngOnInit(): void {
@@ -37,6 +34,7 @@ export class PokemonCardComponent implements OnInit {
   }
 
   getPokemonChosen() {
+    if(this.getPokemon !== '') {
     this.service.getPokemonDetails(this.getPokemon).subscribe((data) => {
       this.pokemon = {
         name: data.name,
@@ -45,18 +43,24 @@ export class PokemonCardComponent implements OnInit {
           (type: { type: { name: string } }) => type.type.name
         ),
         numberId: data.id,
-        statsName: data.stats.map((stat: { stat: { name: string; }; }) => stat.stat.name),
-        statsNumber: data.stats.map((stat: { base_stat: number }) => stat.base_stat.toString())
+        statsName: data.stats.map(
+          (stat: { stat: { name: string } }) => stat.stat.name
+        ),
+        statsNumber: data.stats.map((stat: { base_stat: number }) =>
+          stat.base_stat.toString()
+        ),
       };
-      console.log(data.stats)
       this.pokemons.push(this.pokemon);
 
       localStorage.setItem('pokemons', JSON.stringify(this.pokemons));
     });
+  } else {
+    alert('Preencha o campo de pesquisa!');
   }
+}
 
-  deletePokemons(){
+  deletePokemons() {
     localStorage.removeItem('pokemons');
-    this.pokemons = []
+    this.pokemons = [];
   }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import {PokemonCardComponent} from '../home/pokemon-card/pokemon-card.component'
 
 @Injectable({
   providedIn: 'root',
@@ -13,8 +14,21 @@ export class PokemonService {
     this.baseURL = environment.pokeAPI;
   }
 
+
   getPokemonDetails(pokemon: string): Observable<any> {
     const url = `${this.baseURL}/${pokemon}`;
-    return this.Http.get(url);
+    return this.Http.get(url)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      alert(`Aconteceu um erro por parte do cliente ${error.status}`)
+    } else {
+      alert(`Pokemon não encontrado ${error.status}`)
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
